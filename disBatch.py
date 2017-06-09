flatironinstitute/disBatch

commit afca73aae7cce0cdd40357239f70f3f6bd5a8005

Remove command stripping, as discussed
Potentially breaking change from original behavior

disBatch.py
@@ -223,7 +223,6 @@ def taskGenerator(tasks):
 
         logger.debug('Task: %s', t)
 
-        # Note: intentionally using non stripped line here
         if t.startswith('#DISBATCH '):
             m = dbprefix.match(t)
             if m:
@@ -241,7 +240,7 @@ def taskGenerator(tasks):
                 g = m.group('step')
                 if g: step = int(g)
                 logger.info('Processing repeat: %d %d %d', repeats, rx, step)
-                cmd = prefix + (m.group('command') or '').strip() + suffix
+                cmd = prefix + (m.group('command') or '') + suffix
                 while repeats > 0:
                     yield TaskInfo(taskCounter, tsx, rx, cmd)
                     taskCounter += 1
@@ -259,7 +258,7 @@ def taskGenerator(tasks):
             # Comment or empty line, ignore
             continue
 
-        yield TaskInfo(taskCounter, tsx, 0, prefix + t.strip() + suffix)
+        yield TaskInfo(taskCounter, tsx, 0, prefix + t + suffix)
         taskCounter += 1
 
     logger.info('Processed %d tasks.', taskCounter)

commit d8819f373869ed5ce3997cea4467c6aa21060245

Replace the strip with a selective newline "chomp"
We don't want to include any trailing newlines in the commands (this
introduced a regression from afca73aae7cce0cdd40357239f70f3f6bd5a8005),
but we would like to keep other whitespace.

Tests passing.

disBatch.py
@@ -10,12 +10,12 @@ from threading import BoundedSemaphore, Thread
 myHostname = socket.gethostname()
 myPid = os.getpid()
 
-dbcomment = re.compile('^\s*(#|\n|$)')
-dbbarrier = re.compile('^#DISBATCH BARRIER(?: ([^\n]+)?)?\n', re.I)
+dbcomment = re.compile('^\s*(#|$)')
+dbbarrier = re.compile('^#DISBATCH BARRIER(?: ([^\n]+)?)?$', re.I)
 # would it make sense to allow an (optional) command after the repeat?
-dbrepeat  = re.compile('^#DISBATCH REPEAT\s+(?P<repeat>[0-9]+)(?:\s+start\s+(?P<start>[0-9]+))?(?:\s+step\s+(?P<step>[0-9]+))?(?: (?P<command>[^\n]+))?\s*\n', re.I)
-dbprefix  = re.compile('^#DISBATCH PREFIX ([^\n]+)\n', re.I)
-dbsuffix  = re.compile('^#DISBATCH SUFFIX ([^\n]+)\n', re.I)
+dbrepeat  = re.compile('^#DISBATCH REPEAT\s+(?P<repeat>[0-9]+)(?:\s+start\s+(?P<start>[0-9]+))?(?:\s+step\s+(?P<step>[0-9]+))?(?: (?P<command>[^\n]+))?\s*$', re.I)
+dbprefix  = re.compile('^#DISBATCH PREFIX ([^\n]+)$', re.I)
+dbsuffix  = re.compile('^#DISBATCH SUFFIX ([^\n]+)$', re.I)
 
 # Special ID for "out of band" task events
 TaskIdOOB = -1
@@ -221,6 +221,10 @@ def taskGenerator(tasks):
 
         logger.debug('Task: %s', t)
 
+        # Remove any trailing newline (but avoid stripping any other whitespace)
+        # This allows tasks submitted through kvs with or without newlines or from files (always with newlines)
+        t = t.rstrip('\n')
+
         if t.startswith('#DISBATCH '):
             m = dbprefix.match(t)
             if m:
@@ -247,7 +251,7 @@ def taskGenerator(tasks):
                 continue
             m = dbbarrier.match(t)
             if m:
-                yield BarrierTask(taskCounter, tsx, 0, t[:-1], m.group(1))
+                yield BarrierTask(taskCounter, tsx, 0, t, m.group(1))
                 taskCounter += 1
                 continue
             logger.error('Unknown #DISBATCH directive: %s', t)