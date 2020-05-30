ed6704b756e9e598288fbad4fa763682624126d4
Nicholas Carriero <njcarriero@gmail.com>
2020-05-30 14:21:19 -0400

    Added confirmation popup. Minor cleanup.

--- dbMon.py
+++ dbMon.py
@@ -9,6 +9,8 @@ from kvsstcp import KVSClient
 kvsc = KVSClient(sys.argv[1])
 uniqueId = sys.argv[2]
 
+# TODO: For the moment, we assume the screen is "big enough".
+
 def waitGetch(stdscr, q):
     while True:
         k = stdscr.getch()
@@ -23,17 +25,59 @@ def dbStatus(q):
         except:
             q.put(('stop', None))
             break
-        
-def draw_menu(stdscr):
-    # Clear and refresh the screen for a blank canvas
+
+def popYNC(msg, parent, q, title='Confirm'):
+    ph, pw = parent.getmaxyx()
+    h = int(ph * .75)
+    w = int(pw * .85)
+    ro, co = int((ph - h)*.5), int((pw - w)*.5)
+    l, msgw = '', []
+    for word in msg.split():
+        if len(word) > w:
+            word = word[:w-3] + '...'
+        if len(l) + 1 + len(word) > w:
+            msgw.append(l)
+            l = word
+        else:
+            l = l +  (' ' if l else '') + word
+    msgw.append(l)
+    if len(msgw) > h:
+        missing = 1 + len(msgw) - h
+        msgw = msgw[:h-1]
+        msgw.append('%d lines elided.'%missing)
+
+    nw = curses.newwin(h+2, w+2, ro, co)
+    nw.border()
+    nw.addstr(0, int((w - len(title))*.5), title)
+    for r, l in enumerate(msgw):
+        nw.addstr(r+1, 1, l)
+    nw.addstr(r+2, int((w - len(title))*.5), '[Y]es/[N]o/[C]ancel', curses.A_REVERSE)
+    nw.refresh()
+
+    resp = {'y': 'Y', 'Y':  'y', 'n': 'N', 'N': 'N', 'c': 'C', 'C': 'C'}
+    while True:
+        tag, o = q.get()
+        if tag != 'key': continue
+        k = chr(o)
+        if k in resp:
+            break
+
+    parent.redrawwin()
+    parent.refresh()
+    return resp[k]
+
+FirstEngineRow = 3
+
+def statusWindow(stdscr):
     stdscr.clear()
     stdscr.refresh()
 
-    # Start colors in curses
     curses.start_color()
     curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_RED)
+    curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(5, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.curs_set(False)
     
     q = Queue.Queue()
@@ -43,8 +87,10 @@ def draw_menu(stdscr):
     db = Thread(target=dbStatus, args=(q,))
     db.daemon = True
     db.start()
-
+    
     col, row, done, r2k, statusLine = 0, 0, False, {}, ''
+    cursorLimits = None
+    localEngineStatus = {}
     while (True):
         lRow, lCol = row, col
         tag, o = q.get()
@@ -67,7 +113,7 @@ def draw_menu(stdscr):
                 stdscr.addstr(0, 0, uniqueId + (': {more:15s}  Run{finished:7d}  Failed{failed:7d}  Barriers{barriers:4d}  Total slots{slots:4d}'.format(**d)), curses.color_pair(1))
                 #                       '01234 012345678901 01234567890123456789 0123456789 01234 012345678 0123456 0123456789 0123456789 0123456789'
                 stdscr.addstr(2, 0,     ' Rank    Context           Host            PID      Age     Last    Avail   Assigned   Finished    Failed  ', curses.color_pair(1) | curses.A_UNDERLINE)
-                r, r2k = 3, {}
+                r, r2k = FirstEngineRow, {}
                 ee = sorted(engines.items())
                 for rank, engine in ee:
                     if engine['status'] == 'stopped': continue
@@ -75,8 +121,14 @@ def draw_menu(stdscr):
                     engine['slots'] = len(engine['cylinders'])
                     engine['delay'] = now - engine['last']
                     engine['cLabel'] = contexts[engine['cRank']]['uniqueId']
-                    stdscr.addstr(r, 0, '{rank:5d} {cLabel:12.12s} {hostname:20.20s} {pid:10d} {age:5d} {delay:8.1f}s {slots:7d} {assigned:10d} {finished:10d} {failed:10d}'.format(**engine), curses.color_pair(2))
+                    cp = curses.color_pair(2)
+                    if engine['status'] == 'stopping':
+                        cp = curses.color_pair(5)
+                    elif localEngineStatus.get(rank, '') == 'requesting shutdown':
+                        cp = curses.color_pair(4)
+                    stdscr.addstr(r, 0, '{rank:5d} {cLabel:12.12s} {hostname:20.20s} {pid:10d} {age:5d} {delay:8.1f}s {slots:7d} {assigned:10d} {finished:10d} {failed:10d}'.format(**engine), cp)
                     r += 1
+                cursorLimits = (FirstEngineRow, r if r == FirstEngineRow else r-1)
             except ValueError:
                 stdscr.addstr(0, 0, o, curses.color_pair(1))
         elif tag == 'key':
@@ -89,25 +141,36 @@ def draw_menu(stdscr):
             elif k == curses.KEY_UP:
                 row = row - 1
             elif k == curses.KEY_RIGHT:
-                col = col + 1
+                pass
+                #Ignore horizontal motion for the time being.
+                #col = col + 1
             elif k == curses.KEY_LEFT:
-                col = col - 1
+                pass
+                #col = col - 1
+            elif k == ord('?'):
+                statusLine = 'C: Shutdown context; E: Shutdown engine; q: quit'
             elif k in [ord('C'), ord('E')]:
                 if not done:
-                    #TODO: Add confirmation.
                     target = r2k.get(row, 'No Man')
                     if k == ord('C'):
-                        statusLine = 'Stopping context {cLabel:s} ({cRank:d})'.format(**engines[target]) + ' %d'%row
-                        try:
-                            kvsc.put('.controller', ('stop context', engines[target]['cRank']))
-                        except socket.error:
-                            pass
+                        cRank = engines[target]['cRank']
+                        r = popYNC('Stopping context {cLabel:s} ({cRank:d})'.format(**engines[target]) + ' %d'%row, stdscr, q)
+                        if r == 'Y':
+                            try:
+                                kvsc.put('.controller', ('stop context', cRank))
+                                for rank, e in engines.items():
+                                    if e['cRank'] == cRank:
+                                        localEngineStatus[rank] = 'requesting shutdown'
+                            except socket.error:
+                                pass
                     elif k == ord('E'):
-                        statusLine = 'Stopping engine {rank:d} ({hostname:s}, {pid:d})'.format(**engines[target])
-                        try:
-                            kvsc.put('.controller', ('stop engine', target))
-                        except socket.error:
-                            pass
+                        r = popYNC('Stopping engine {rank:d} ({hostname:s}, {pid:d})'.format(**engines[target]), stdscr, q)
+                        if r == 'Y':
+                            try:
+                                kvsc.put('.controller', ('stop engine', target))
+                                localEngineStatus[target] = 'requesting shutdown'
+                            except socket.error:
+                                pass
             else:
                 statusLine = 'Last unrecognized key %d'%k
         elif tag == 'stop':
@@ -116,11 +179,11 @@ def draw_menu(stdscr):
         else:
             raise Exception('Unknown tag: '+tag)
 
-        col = max(0, col)
-        col = min(width-1, col)
-
-        row = max(3, row)
-        row = min(height-1, row)
+        if cursorLimits is not None:
+            row = max(cursorLimits[0], row)
+            row = min(cursorLimits[1], row)
+        else:
+            row = FirstEngineRow
 
         stdscr.move(height-1, 0)
         stdscr.clrtoeol();
@@ -133,7 +196,7 @@ def draw_menu(stdscr):
         stdscr.refresh()
 
 def main():
-    curses.wrapper(draw_menu)
+    curses.wrapper(statusWindow)
 
 if __name__ == "__main__":
     main()
