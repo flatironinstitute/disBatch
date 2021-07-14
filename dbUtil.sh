38c838b3c930967ee75876c7e27d05b21665de60
Nicholas Carriero <njcarriero@gmail.com>
2021-07-14 17:12:09 -0400

    Moved KVS connection info to an environment variable. Added engine wrapper.

--- dbUtil.sh
+++ dbUtil.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-DISBATCH_ROOT={dbRoot:s}
-KVSSTCP_HOST={kvsserver:s}
+export DISBATCH_KVSSTCP_HOST={kvsserver:s} DISBATCH_ROOT={DbRoot:s}
 
 if [[ $1 == '--mon' ]]
 then
-    ${{DISBATCH_ROOT}}/dbMon.py ${{KVSSTCP_HOST}} {uniqueId:s}
+    exec ${{DISBATCH_ROOT}}/dbMon.py {uniqueId:s}
+elif [[ $1 == '--engine' ]]
+then
+    exec ${{DISBATCH_ROOT}}/disBatch.py "$@"
 else
-    ${{DISBATCH_ROOT}}/disBatch.py --context ${{KVSSTCP_HOST}} "$@" < /dev/null &> {uniqueId:s}_${{BASHPID}}_context_launch.log
+    exec ${{DISBATCH_ROOT}}/disBatch.py --context {DbUtilPath:} "$@" < /dev/null &> {uniqueId:s}_${{BASHPID}}_context_launch.log
 fi
