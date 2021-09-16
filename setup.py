3158792c8231667233b3cfeefdd25fe61cf30bba
Robert Blackwell <rblackwell@scclin016.flatironinstitute.org>
2021-09-16 15:26:00 -0400

    Point pip to kvsstcp dependency on github, change script to symlink
    Pip will autofix the symlink to a full file when doing a pip install

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 setup(name='disbatch', version='0',
     description="Distributed processing of a batch of tasks",
     long_description=open("Readme.md").read(),
-    requires=['kvsstcp'],
-    scripts=['disBatch.py'])
+    install_requires=['kvsstcp @ git+ssh://git@github.com/flatironinstitute/kvsstcp@1.2#egg=kvsstcp'],
+    scripts=['disBatch'])
