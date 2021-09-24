50b693d141da4a6c19a212438ca3592abef02982
Robert Blackwell <rblackwell@scclin016.flatironinstitute.org>
2021-09-24 10:04:41 -0400

    Correct path in dbUtil.sh for pip installs

--- disbatch/dbUtil.sh
+++ disbatch/dbUtil.sh
@@ -2,6 +2,12 @@
 
 export DISBATCH_KVSSTCP_HOST={kvsserver:s} DISBATCH_ROOT={DbRoot:s}
 
+# This should find the correct path to disBatch for pip installs
+if [ ! -f "$DISBATCH_ROOT/disBatch.py" ]
+then
+    export DISBATCH_ROOT="$DISBATCH_ROOT/../../../../bin"
+fi
+
 if [[ $1 == '--mon' ]]
 then
     exec ${{DISBATCH_ROOT}}/dbMon.py {uniqueId:s}
