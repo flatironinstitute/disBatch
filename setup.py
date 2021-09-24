79b994aca7435b5d0f2dc5be96bfebdf3dd20dba
Robert Blackwell <rblackwell@scclin016.flatironinstitute.org>
2021-09-24 10:04:24 -0400

    Add dbMon.py to scripts

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setuptools.setup(name='disbatch', version='2.0-RC3',
     description="Distributed processing of a batch of tasks",
     long_description=open("Readme.md").read(),
     install_requires=['kvsstcp @ git+ssh://git@github.com/flatironinstitute/kvsstcp@1.2#egg=kvsstcp'],
-    packages=setuptools.find_packages(exclude=["kvsstcp"]),
+    packages=['disbatch'],
     package_data={'disbatch': ['dbUtil.sh']},
-    scripts=['disBatch', 'disBatch.py'])
+    scripts=['disBatch', 'disBatch.py', 'dbMon.py'])
