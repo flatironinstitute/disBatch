fd53b03c130c50f8f06da450ea66b0fd71047f0c
Nicholas Carriero <njcarriero@gmail.com>
2021-12-16 14:06:42 -0500

    Addressing RB's PR review comments.

--- disBatch.py
+++ disBatch.py
@@ -2,11 +2,9 @@
 
 import os, sys
 
-dr = None
-if 'DISBATCH_ROOT' in os.environ:
-    dr = os.environ['DISBATCH_ROOT']
-    if dr not in sys.path:
-        sys.path.append(dr)
+dr = os.getenv('DISBATCH_ROOT')
+if dr and dr not in sys.path:
+    sys.path.append(dr)
         
 try:
     import disbatch
@@ -14,5 +12,5 @@ except:
     print(f'disBatch environment is incomplete. Check:\n\tDISBATCH_ROOT {dr!r}.', file=sys.stderr)
     sys.exit(1)
 
-dbExec = os.path.join(os.path.sep.join(disbatch.__file__.split(os.path.sep)[:-1]), 'disBatch.py')
+dbExec = os.path.join(os.path.dirname(disbatch.__file__), 'disBatch.py')
 os.execv(sys.executable, [sys.executable, dbExec] + sys.argv[1:])
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import setuptools
 
-setuptools.setup(name='disbatch', version='2.0-RC3',
+setuptools.setup(name='disbatch', version='2.1',
     description="Distributed processing of a batch of tasks",
     long_description=open("Readme.md").read(),
     packages=setuptools.find_packages(),
