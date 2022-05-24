bb64d4a37bd05c592d8079a1dd527d396fbacebd
ygliu2016 <yanbin_liu@yahoo.com>
2022-05-24 16:13:54 -0400

    m2r2

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ sys.path.insert(0, parent)
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx_rtd_theme', 'sphinx.ext.githubpages']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx_rtd_theme', 'sphinx.ext.githubpages', 'm2r2']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
